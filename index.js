46ef42590c0b2b90e47f4586f26ef5e35ee01afe
Flarp <flurpdadurp@gmail.com>
2017-06-27 17:17:06 -0400

    Start with comments

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const acorn = require('acorn/dist/acorn_loose.js')
+const walk = require('acorn/dist/walk.js')
 browser.webRequest.onBeforeRequest.addListener(request => {
   return new Promise((resolve, reject) => {
      fetch(request.url)
     .then(res => res.text())
     .then(text => {
-      console.log(acorn.parse_dammit(text))
+      /*walk.simple(acorn.parse_dammit(text), {
+        CallExpression(node) {
+          if (node.callee.name === "eval") {
+            console.log(node)
+          }
+        }
+      })
+      */
+      acorn.parse_dammit(text, { onComment: (x, c) => {
+        console.log(c)
+      } })
       resolve({ cancel: false })
     })
   })
